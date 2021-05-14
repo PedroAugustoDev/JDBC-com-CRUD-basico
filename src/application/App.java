6611ca7febd63290a56b2c000f9fce1767732638
PedroAugustoDev <74946703+PedroAugustoDev@users.noreply.github.com>
2021-05-14 02:47:17 -0300

    Mudei o usuario rs

--- src/application/App.java
+++ src/application/App.java
@@ -10,7 +10,7 @@ public class App {
     public static void main(String[] args) throws SQLException {
         DAO dao = new DAO();
         //new user
-        User user = new User("Carlito Brinho", "carlos@gmail.com");
+        User user = new User("Pedro Augusto", "pedro@gmail.com");
         //insert -> Create
         if(dao.insertUser(user) > 0){
             System.out.println("usuário adicionado com sucesso");
